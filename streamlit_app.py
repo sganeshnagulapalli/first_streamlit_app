c4ea5ec1996ce2b96d9fcbc77fc6bc9208d50c24
sganeshnagulapalli <133891669+sganeshnagulapalli@users.noreply.github.com>
2023-05-29 11:38:51 +0200

    Update streamlit_app.py
    added fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,12 +17,17 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
 #New section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # Deleting below line
 #streamlit.text(fruityvice_response.json())
 
