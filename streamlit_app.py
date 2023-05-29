b7feb7be5517f032d807a6bf5aabfe157d851271
sganeshnagulapalli <133891669+sganeshnagulapalli@users.noreply.github.com>
2023-05-29 11:35:54 +0200

    Update streamlit_app.py
    delete json container

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,9 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# Deleting below line
+#streamlit.text(fruityvice_response.json())
 
 # normalizing JSON responses
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
