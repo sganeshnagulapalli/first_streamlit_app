ba56ec3c5ac9f1fb52c264afe9288f187703dda9
sganeshnagulapalli <133891669+sganeshnagulapalli@users.noreply.github.com>
2023-05-29 17:33:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,18 +24,22 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choiice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+#  streamlit.write('The user entered ', fruit_choice)
 # Deleting below line
 #streamlit.text(fruityvice_response.json())
 
-# normalizing JSON responses
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Sending normalized data to streamlit
-streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.stop()
 
@@ -46,7 +50,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
 
 fruit_choice = streamlit.text_input('What fruit would you liketo add','jackfruit')
 streamlit.write('The user entered ', fruit_choice)
