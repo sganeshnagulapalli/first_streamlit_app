b43a4b95c4fe66705edcd6874a1736e91e0faf7d
sganeshnagulapalli <133891669+sganeshnagulapalli@users.noreply.github.com>
2023-05-29 17:16:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,12 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+fruit_choice = streamlit.text_input('What fruit would you liketo add','jackfruit')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# Deleting below line
+#streamlit.text(fruityvice_response.json())
