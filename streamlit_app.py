2501d3fe90876e34eddf914c01d4ed9d848e6c6e
sganeshnagulapalli <133891669+sganeshnagulapalli@users.noreply.github.com>
2023-05-29 17:21:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,5 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # Deleting below line
 #streamlit.text(fruityvice_response.json())
+
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
